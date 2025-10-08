412a90fc1caf10ca02425c3e2c6030749fa5ad55
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-08 17:48:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
+// Controle de seções e transição fade
 const buttons = document.querySelectorAll(".menu-btn");
 const sections = document.querySelectorAll(".section");
 
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
-    // Remove active de todos
     buttons.forEach(b => b.classList.remove("active"));
-    sections.forEach(sec => {
-      sec.classList.remove("active");
-    });
+    sections.forEach(sec => sec.classList.remove("active"));
 
-    // Ativa o clicado
     btn.classList.add("active");
     const target = document.getElementById(btn.dataset.section);
-
-    // Fade suave
     target.classList.add("active");
   });
 });

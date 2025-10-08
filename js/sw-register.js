781872d7eaa3f1219a42b321bdb89d7ce8886ca3
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-08 18:06:38 -0300

    Update sw-register.js

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,7 +1,8 @@
-// Registro do Service Worker para o PWA
+// js/sw-register.js
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("../service-worker.js")
+    .register("/espaco-zmagalhaes/service-worker.js")
     .then(() => console.log("✅ Service Worker registrado com sucesso"))
     .catch(err => console.log("⚠️ Erro ao registrar o Service Worker:", err));
 }
